37761c1f089e7d409dabb3255d539e31acd6ab56
phiarchitect <phi@phiarchitect.com>
2023-05-06 08:30:01 -0700

    debug

---
+++ src/photonplatform/logger/app.py
@@ -0,0 +1,28 @@
+"""The main application class for the viewer."""
+
+#  from argparse import ArgumentParser, Namespace
+#  from webbrowser import open as open_url
+
+#  from textual import __version__ as textual_version  # pylint: disable=no-name-in-module
+#  from textual.app import App
+
+#  from .. import __version__
+#  from ..data import load_config
+#  from ..screens import Main
+#  from ..utility.advertising import APPLICATION_TITLE, PACKAGE_NAME
+
+from .logger import Logger
+
+
+def run() -> None:
+    """Run the application."""
+    #  CalculatorApp().run()
+    #  import subprocess
+
+    print("logger")
+    #  Logger().run()
+    #  reply = Logger().run()
+    #  print(reply)
+
+    #  if reply:
+    #  subprocess.run(['vim', reply])
